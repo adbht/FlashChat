15792c6757d64513d2771f3d60d22c24786ce2e6
Aditya Bhatia <30832959+adbht@users.noreply.github.com>
2018-07-29 00:56:33 -0400

    Add files via upload

---
+++ FlashChat/Model/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  Created by Aditya Bhatia on 2018-07-24.
+//  Copyright (c) 2018 adbht. All rights reserved.
+//
+
+class Message {
+    var sender = String()
+    var messageBody = String()
+}

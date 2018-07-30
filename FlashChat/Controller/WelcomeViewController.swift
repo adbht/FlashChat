798849d55ee1d5d95cf24f65a4acb7fdf6fad467
Aditya Bhatia <30832959+adbht@users.noreply.github.com>
2018-07-30 10:39:47 -0400

    Add files via upload

---
+++ FlashChat/Controller/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat
+//
+//  Created by Aditya Bhatia on 2018-07-24.
+//  Copyright (c) 2018 adbht. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}

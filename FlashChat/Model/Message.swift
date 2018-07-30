fd51b441c4e8809700fe72e5b3a71272414ce92e
Aditya Bhatia <30832959+adbht@users.noreply.github.com>
2018-07-30 10:54:29 -0400

    Update Message.swift

--- FlashChat/Model/Message.swift
+++ FlashChat/Model/Message.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2018 adbht. All rights reserved.
 //
 
-class Message {
-    var sender = String()
-    var messageBody = String()
+class Message {                     //This class will represent the model of the project
+    var sender = String()           //This property is required to store the username of the user
+    var messageBody = String()      //This property is required to store the message the user has typed
 }

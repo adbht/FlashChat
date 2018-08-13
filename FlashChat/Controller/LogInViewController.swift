ef2187502d344bc82f3394d487b4252372e77a1b
Aditya Bhatia <30832959+adbht@users.noreply.github.com>
2018-08-13 10:41:33 -0400

    Update LogInViewController.swift

--- FlashChat/Controller/LogInViewController.swift
+++ FlashChat/Controller/LogInViewController.swift
@@ -16,23 +16,23 @@ class LogInViewController: UIViewController {
     @IBOutlet var emailTextfield: UITextField!      //Textfield where user inputs their email address
     @IBOutlet var passwordTextfield: UITextField!   //Textfield where user inputs their password
     
+    @IBAction func logInPressed(_ sender: AnyObject) {  //This function is executed when the user clicks on the Log In button
+        SVProgressHUD.show()                            //To display the loading animated wheel so the user knows that the application is trying to log in to the user's database
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in //This closure is used to check if the the user's inputted credentials matched with Firebase's existing user accounts
+            if error != nil {
+                SVProgressHUD.showError(withStatus: "Incorrect email address or password. Please try again.")   //Display an error HUD if log in was unsuccessful. This can happen due to incorrect user credentials or no network connectivity
+            } else {
+                SVProgressHUD.dismiss() //If the log in was successful, stop displaying the loading wheel as the application has accessed user's database
+                self.performSegue(withIdentifier: "goToChat", sender: self) //Finally, the user should be directed to the chat view with thie existing messages displayed
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-
-    @IBAction func logInPressed(_ sender: AnyObject) {  //This function is executed when the user clicks on the Log In button
-        SVProgressHUD.show()                            //To display the loading animated wheel so the user knows that the application is trying to log in to the user's database
-        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in //This closure is used to check if the the user's inputted credentials matched with Firebase's existing user accounts
-            if error != nil {
-                SVProgressHUD.showError(withStatus: "Incorrect email address or password. Please try again.")   //Display an error HUD if log in was unsuccessful. This can happen due reasons such as incorrect user credentials or no network connectivity
-            } else {
-                SVProgressHUD.dismiss() //If the log in was successful, stop displaying the loading wheel as the application has accessed user's database
-                self.performSegue(withIdentifier: "goToChat", sender: self) //Finally, upon successful log in, the user should be directed to the chat view with thie existing messages displayed
-            }
-        }
     }
 }  
